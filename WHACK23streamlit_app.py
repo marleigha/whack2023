8956871d86d2a16ff3cb25335d98f61f4d527961
marleigha <118072178+marleigha@users.noreply.github.com>
2023-11-04 12:33:06 -0400

    Update WHACK23streamlit_app.py

--- WHACK23streamlit_app.py
+++ WHACK23streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+#importing the pieces streamlit had in their script
+
+#title is pending
+st.title('Cultural Events')
+
 print("hello is this good?")

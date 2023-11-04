5b3dd85695c26e2e70c1b1667388d75fb8ce831f
marleigha <118072178+marleigha@users.noreply.github.com>
2023-11-04 15:29:23 -0400

    testing again!

--- WHACK23streamlit_app.py
+++ WHACK23streamlit_app.py
@@ -4,6 +4,17 @@ import numpy as np
 #importing the pieces streamlit had in their script
 
 #title is pending
-st.title('Cultural Events')
+st.title('Culture Atlas')
 
-print("hello is this good?")
+#this is just me putting in stuff from the s
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
